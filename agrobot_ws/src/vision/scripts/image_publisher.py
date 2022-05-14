8b12744467a293af4b4a259f0cc7ba9e99dcacf8
Sijt-H <s.j.c.hooghwinkel@student.tue.nl>
2022-05-14 16:27:19 +0200

    Created improved detection

--- agrobot_ws/src/vision/scripts/image_publisher.py
+++ agrobot_ws/src/vision/scripts/image_publisher.py
@@ -4,22 +4,22 @@ import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 import cv2
-
-cap = cv2.VideoCapture(0) #2 = webcam, 0 = front facing camera
-print(cap.isOpened())
+cam_num = input("Give camera number, 0 or 2: ") #0 is default
+cap = cv2.VideoCapture(int(cam_num)) #image captured
+print(cap.isOpened()) #camera can be accessed
 bridge = CvBridge()
 
 def talker():
-    pub = rospy.Publisher('/image', Image, queue_size = 1)
-    rospy.init_node('webcam', anonymous = False)
-    rate = rospy.Rate(10)
+    pub = rospy.Publisher('/image', Image, queue_size = 1) #create topic
+    rospy.init_node('webcam', anonymous = False) #create node
+    rate = rospy.Rate(20) #rate [Hz]
     while not rospy.is_shutdown():
         ret,frame = cap.read()
         if not ret:
             break
         
-        msg = bridge.cv2_to_imgmsg(frame, "bgr8")
-        pub.publish(msg)
+        msg = bridge.cv2_to_imgmsg(frame, "bgr8") #convert image to ROS image
+        pub.publish(msg) #publish image
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
