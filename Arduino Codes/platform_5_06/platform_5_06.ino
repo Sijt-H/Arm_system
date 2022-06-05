7d18fc478834b2c6025b78ea00944f5910fe0af3
Sijt-H <s.j.c.hooghwinkel@student.tue.nl>
2022-06-05 18:29:17 +0200

    refined arduino code

--- Arduino Codes/platform_5_06/platform_5_06.ino
+++ Arduino Codes/platform_5_06/platform_5_06.ino
@@ -1,26 +1,21 @@
 #include <AccelStepper.h>
-
-//Pins
+#include <math.h>
 #define dirPin 2 //Digital output pin
 #define stepPin 3 //Digital output pin
-#define home_switch 9 //Micro-switch used for homing the platform
-
-
 #define motorInterfaceType 1 //Easy driver interface
 AccelStepper platform = AccelStepper(motorInterfaceType, stepPin, dirPin);
-
-// Constants
+#define home_switch 9 //Micro-switch used for homing the platform
 #define revolution 200 // Steps per full revolution, step angle NEMA17 = 1.8 deg -> 200 steps per 360 deg
 #define radius 0.62 //Radius [cm] of the pulley mounted on the NEMA17, r = 2*d
-#define platformRange 6000 //Maximum steps travel range of the platfrom from homing position 
+
 
 // Globals
 int move_finished = 1; // Used to check if move is completed
 long initial_homing = -1; // Used to Home Stepper at startup
-bool homing_completed = false;
 int input;
 float cropX;
-float PixelX[] = {15,50,90}; //X-coordinate of pixels [cm] ([0] = Pixel 1 (most leftern pixel), [1] = Pixel 2 (middle pixel), [2] = Pixel 3 (closest to homing position))
+int platformRange = 6000; //Maximum steps travel range of the platfrom from homing position
+float PixelX[] = {0.15,0.5,0.9}; //X-coordinate of pixels [cm] ([0] = Pixel 1 (most leftern pixel), [1] = Pixel 2 (middle pixel), [2] = Pixel 3 (closest to homing position))
 //fix: convert to steps
 
 
@@ -54,15 +49,16 @@ void subscriberCallback() {
 }
 
 int Conversion(float X){
-  // 100cm = 5030 steps // measured from homed 0 point to right side of the platform
+  // 100cm = 5030 steps // gemeten van 0 punt tot rechter punt platform.
   //Conversion of X coordinates [cm] to number of steps [-]
   int steps;
   int stepsperm = 5030; //lmao
-  steps = X/100*stepsperm;
+  steps = X*stepsperm;
     Serial.print("Steps: ");
-  Serial.print(steps);
-  
-  if(steps>platformRange){
+  Serial.println(steps);
+  Serial.print("X: ");
+  Serial.println(X);
+  if(steps>6000){
     Serial.print("Out of bounds, limit to frame");
     return 5030;
     }
@@ -81,7 +77,7 @@ void MoveToPixel(){ //moves the platform to Pixel 1,2 or 3
 
 void MoveToCrop(){ //Moves the platform to the crop, based on the output of the mapping.py script
   
-  Serial.println("Input the crop X-coordinate [cm]: ");
+  Serial.println("Input the crop X-coordinate [m]: ");
   while (Serial.available() == 0) {
     // Wait for input
   }
@@ -132,27 +128,16 @@ void StepperHoming() { //Function that does the homing sequence
 void setup()
 {
   pinMode(home_switch, INPUT_PULLUP);
+  //StepperHoming(); //Initial homing using the micro-switch
+  
   Serial.begin(115200);
   delay(10);
 
-  //after every restart of the Mega, a homing sequence needs to be done
-  Serial.println("Type 0 to start homing sequence: ");
-  int homing_confirm = 2;
-  while (homing_confirm != 0){
-    while (Serial.available() == 0) {
-    // Wait for input
-  }
-  homing_confirm = Serial.parseInt(); //user inputs a value to confirm homing sequence initiation
-    
-    }
-    //StepperHoming(); 
-    homing_completed = true;
 }
 
 void loop()
 {
-  Serial.println("-----------------------------");
-  Serial.println("Menu: ");
+  Serial.println("Input any number: ");
   Serial.println("0 = Homing sequence");
   Serial.println("1 = Move to Pixel 1");
   Serial.println("2 = Move to Pixel 2");
@@ -167,7 +152,7 @@ void loop()
   Serial.println(input);
   Serial.println(" ");
   
-  if(input > 4){ //if not any of the given options
+  if(input > 4){
     Serial.println("Invalid input");
     }
   else{
--- README.md
+++ README.md
@@ -4,54 +4,7 @@ The PSU has the potential to be lethal if misused.
 
 Double check your work and proceed with care!
 
-
-
-
-# Arm_system
-System: Ubuntu 20.04 (running as dualboot not in a VM), ROS Noetic
-## Packages
-For every sub-system a ROS package has been created. These packages are interconnected by publishing and subscribing to the relevant topics. 
-
-### Vision
-`image_publisher.py` opens the webcam connection, creates a `\webcam` node and publishes the image on the `image` topic.
-The image publisher asks for a camera number to select. On a device with multiple cameras (eg. forward-facing and external USB), each camera has a number. 
-
-cd to `~/Arm_sytem/support_scripts/` and run `python3 camera_selection.py`. This will output the numbers of the connected cameras you can choose from. 
-
-Number 0 is reserved for the default camera, which would be the front-facing camera for most laptops. After a restart the last used camera is set to be default.
-
-### Detection
-The scripts `detection.py` subscribes to `image` topic. The image received is a ROS type image and needs to be converted to an OpenCV type image to be usable.
-Using Hough Circle Transform the crop is detected and the coordinates of the center point of the circle are published to be mapped.
-
-The detection package also contains `mapping.py`, this script takes the image coordinates that are detected and maps them to the real world coordinates. 
-
-Some setup needs to be done to use the mapping script:
-
-#### Mapping setup
-Calibrate the camera
-Measure world points and corresponsing image points
-put the image points, world points in the ... script.
-Correct the load directory `loaddir` in `mapping.py`
-#### Mapping
-The mapping calculates the optimal scaling factor "s" for the image and world points that are measured. Using the intrinsic camera parameters: camera matrix and distortion matrix, the image coordinates can be mapped to world coordinates. 
-
-## Arduino
-The Arduino Mega is connected to the stepper driver (A4988) and controls it using two outputs, STEP and DIR.
-
-## Running with ROS
-Complete the following commands to run the system using ROS.
-
-```
-cd to ~Arm_system\agrobot_ws\
-catkin_make *only after changes have been made to any packages*
-roscore
-rosrun vision image_publisher.py
-rosrun detection detection.py
-rosrun detection mapping.py
-rosrun rosserial_python serial_node.py /dev/ttyACM0
-```
-
+--Read the WIKI attached to the Github repository.--
 
 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
    
